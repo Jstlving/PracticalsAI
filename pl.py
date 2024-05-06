4b41e6d96b96f303bc06ba2da50acfc89fe5c0b3
Jstlving <168092173+Jstlving@users.noreply.github.com>
2024-05-06 06:13:28 +0530

    Create pl.py

---
+++ pl.py
@@ -0,0 +1,17 @@
+p = True
+q = False
+
+# Negation
+print(not p)
+
+# Conjunction
+print(p and q)
+
+# Disjunction
+print(p or q)
+
+# Implication
+print(not p or q)
+
+# Biconditional
+print((not p or q) and (not q or p))
